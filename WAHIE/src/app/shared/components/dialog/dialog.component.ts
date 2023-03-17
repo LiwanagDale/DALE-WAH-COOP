2c67a64b87f62ee110d90ab28b045441f38bd897
CjCedddy <ced22ced22ced@gmail.com>
2023-03-17 16:59:17 +0800

    Benificiary Add Button and Components

--- WAHIE/src/app/shared/components/dialog/dialog.component.ts
+++ WAHIE/src/app/shared/components/dialog/dialog.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class DialogComponent {
 
+  row = [
+    {
+      id : '',
+      name: '',
+      email: ''
+    },
+    {
+      id : '',
+      name: '',
+      email: ''
+    },
+    {
+      id : '',
+      name: '',
+      email: ''
+    }
+  ];
+  
+  addTable() {
+    const obj = {
+      id: '',
+      name: '',
+      email: ''
+    }
+    this.row.push(obj)
+  }
+  
+  deleteRow(x: number){
+    var delBtn = confirm(" Do you want to delete ?");
+    if ( delBtn == true ) {
+      this.row.splice(x, 1 );
+    }   
+  } 
+          
+  // deleteRow (){
+  //   //console.log(name);
+  
+  //   for(var i=0 ; i<this.row.length-1 ; i++){
+  //     var delBtn = confirm(" Do you want to delete ?");     
+  //            if( delBtn == true ) {
+     
+  //       this.row.splice(i,1);
+  //     }
+  //   }
+  
+  // }
+  // }
+  
+
 }
