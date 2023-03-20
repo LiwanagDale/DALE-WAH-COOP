b66edc73a718edea301fe95304273c136f34667b
LiwanagDale <111510725+LiwanagDale@users.noreply.github.com>
2023-03-20 08:48:01 +0800

    dale march 20

--- WAHIE/src/app/journal/journal.component.ts
+++ WAHIE/src/app/journal/journal.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./journal.component.css']
 })
 export class JournalComponent {
+  row = [
+    {
+      id : '',
+      name: '',
+      email: ''
+    },
+    {
+      id : '',
+      name: '',
+      email: ''
+    },
+    {
+      id : '',
+      name: '',
+      email: ''
+    }
+  ];
+  
+  addTable() {
+    const obj = {
+      id: '',
+      name: '',
+      email: ''
+    }
+    this.row.push(obj)
+  }
+  
+  deleteRow(x: number){
+    var delBtn = confirm(" Do you want to delete ?");
+    if ( delBtn == true ) {
+      this.row.splice(x, 1 );
+    }   
+  } 
 
 }
